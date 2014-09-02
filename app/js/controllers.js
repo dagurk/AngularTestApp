9f7ffb89ad84ca2c70718e4b72f4f22a7de1110e
Dagur Kristinsson <dagurk@gmail.com>
2014-09-02 22:21:03 +0000

    Lesson 37

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,47 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('LandingPageController', [function() {
 
   }])
-  .controller('MyCtrl2', [function() {
+  .controller('WaitlistController', ['$scope', 'partyService', 'textMessageService', 'authService', function($scope, partyService, textMessageService, authService) {
 
-  }]);
+  	// Bind users parties to $scope.parties.
+  	authService.getCurrentUser().then(function(user) {
+  		if(user) {
+  			$scope.parties = partyService.getPartiesByUserId(user.id);
+  		};
+  	});
+
+  	// Object to store data from the waitlist form.
+  	$scope.newParty = {name: '', phone: '', size: '', done: false, notified: 'No'};
+
+  	// Function to save a new party to the waitlist.
+  	$scope.saveParty = function() {
+  		partyService.saveParty($scope.newParty, $scope.currentUser.id);
+  		// Reset the newParty variable
+	  	$scope.newParty = {name: '', phone: '', size: '', done: false, notified: 'No'};
+  	};
+
+  	// Funtino to send text message to a party.
+  	$scope.sendTextMessage = function(party) {
+  		textMessageService.sendTextMessage(party, $scope.currentUser.id);
+  	};
+  }])
+	.controller('AuthController', ['$scope', 'authService', function($scope, authService) {
+
+		// Objcet bond to inputs on the register and login pages.
+		$scope.user = {email: '', password: ''};
+
+		$scope.register = function() {
+			authService.register($scope.user);
+		};
+
+		$scope.login = function() {
+			authService.login($scope.user);
+		};
+		$scope.logout = function() {
+			authService.logout();
+		};
+
+	}]);
